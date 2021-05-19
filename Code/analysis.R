a8e54e3189b7a3e3e32cf9b65f9f71b3adc0542e
Ruth Keane <RKe@socrates.local>
2021-05-19 14:39:35 +0100

    Revert " most recent"
    This reverts commit 66555c170a8a69fc031668722aa4dcd8fab52fcc.

--- Code/analysis.R
+++ Code/analysis.R
@@ -4,28 +4,6 @@
 require(ggplot2)
 require(pROC)
 
-
-#function
-#function which given a significance level, calculates specificity and sensitivity
-get_spec_sens<-function(data_mod,significance){
-  #boolean- is absolute value above significance value
-  data_mod$mono_significance<-abs(data_mod$monothermal_screen)>=significance
-  
-  # for the rows of data_mod where overall significance is true AND mono significance is true - set rating to TP
-  data_mod[which(data_mod$overall_significance&data_mod$mono_significance),"rating"]<-"TP"
-  # for the rows of data_mod where overall significance is NOT true (!) AND mono significance is true - set rating to FP
-  data_mod[which(!data_mod$overall_significance&data_mod$mono_significance),"rating"]<-"FP"
-  # for the rows of data_mod where overall significance is true AND mono significance is NOT true - set rating to FN
-  data_mod[which(data_mod$overall_significance&!data_mod$mono_significance),"rating"]<-"FN"
-  # for the rows of data_mod where overall significance is NOT true (!) AND mono significance is NOT true - set rating to TN
-  data_mod[which(!data_mod$overall_significance&!data_mod$mono_significance),"rating"]<-"TN"
-  #calculate sensitivity  and specificty from our ratings column
-  sensitivity<-sum(data_mod$rating=="TP",na.rm = T)/(sum(data_mod$rating=="TP",na.rm = T)+sum(data_mod$rating=="FN",na.rm = T))
-  specificity<-sum(data_mod$rating=="TN",na.rm = T)/(sum(data_mod$rating=="TN",na.rm = T)+sum(data_mod$rating=="FP",na.rm = T))
-  #return sensitivity and specificity
-  #we don't return the dataset because we don't need the modified dataset anymore
-  return(c(sensitivity,specificity))
-}
 ##read in data
 
 #done in a very messy way, because I didn't manually edit the excel doc and the formatting was weird
@@ -36,15 +14,13 @@ data_raw<-read.csv("../Data/data.csv", skip =3)
 #set colnames for dataset using col_names dataframe
 colnames(data_raw)<-col_names[2,]
 #replace 0 with NA
-#data_raw[data_raw == 0] <- NA
+data_raw[data_raw == 0] <- NA
 #remove empty cells
 data_raw<-data_raw[1:800,1:5]
 
 #data_mod is the dataset we're adding to
 data_mod<-data_raw
 
-
-likelihood_ratio_dataset<-function(data_mod){
 #make son columns with helpful additions done to make the formulae less messy!
 data_mod$sum_lefts<-data_mod$WL+data_mod$CL
 data_mod$sum_rights<-data_mod$WR+data_mod$CR
@@ -79,9 +55,9 @@ data_mod[selC,"monothermal_screen"]<-(data_mod[selC,"diff_cools"]/data_mod[selC,
 
 #Boolean variable- tels us whether the statement is true or false. so below asks is the absolute value of canal paresis >20
 #is canal paresis significant
-data_mod$CP_significance<-abs(data_mod$canal_paresis)>=20
+data_mod$CP_significance<-abs(data_mod$canal_paresis)>20
 #is directional proponderance significant
-data_mod$DP_significance<-abs(data_mod$directional_proponderance)>=20
+data_mod$DP_significance<-abs(data_mod$directional_proponderance)>20
 # is canal paresis and/or directional proponderance significant.
 data_mod$overall_significance<-data_mod$CP_significance | data_mod$DP_significance
 
@@ -90,8 +66,26 @@ data_mod$rating<-NA
 #make empty column for whether the monothermal screening result is positive or negative
 data_mod$mono_significance<-NA
 
-
-
+#function which given a significance level, calculates specificity and sensitivity
+get_spec_sens<-function(data_mod,significance){
+  #boolean- is absolute value above significance value
+  data_mod$mono_significance<-abs(data_mod$monothermal_screen)>significance
+  
+  # for the rows of data_mod where overall significance is true AND mono significance is true - set rating to TP
+  data_mod[which(data_mod$overall_significance&data_mod$mono_significance),"rating"]<-"TP"
+  # for the rows of data_mod where overall significance is NOT true (!) AND mono significance is true - set rating to FP
+  data_mod[which(!data_mod$overall_significance&data_mod$mono_significance),"rating"]<-"FP"
+  # for the rows of data_mod where overall significance is true AND mono significance is NOT true - set rating to FN
+  data_mod[which(data_mod$overall_significance&!data_mod$mono_significance),"rating"]<-"FN"
+  # for the rows of data_mod where overall significance is NOT true (!) AND mono significance is NOT true - set rating to TN
+  data_mod[which(!data_mod$overall_significance&!data_mod$mono_significance),"rating"]<-"TN"
+  #calculate sensitivity  and specificty from our ratings column
+  sensitivity<-sum(data_mod$rating=="TP",na.rm = T)/(sum(data_mod$rating=="TP",na.rm = T)+sum(data_mod$rating=="FN",na.rm = T))
+  specificity<-sum(data_mod$rating=="TN",na.rm = T)/(sum(data_mod$rating=="TN",na.rm = T)+sum(data_mod$rating=="FP",na.rm = T))
+  #return sensitivity and specificity
+  #we don't return the dataset because we don't need the modified dataset anymore
+  return(c(sensitivity,specificity))
+}
 
 #set min and max significances 
 min_sig=0
@@ -123,11 +117,11 @@ for (i in 1:length(sig_range)){
 sens_spec_dataset$false_pos_rate<- 1 - sens_spec_dataset$specificity
 
 #make ROC plot (ggplot)
-g<-ggplot(data=sens_spec_dataset,aes(x=false_pos_rate,y=sensitivity))+
+g<-ggplot(data=sens_spec_dataset,aes(false_pos_rate,sensitivity))+
   geom_text(label=sens_spec_dataset$significance,nudge_x = 0.05, nudge_y = 0.05, check_overlap = T)+
-  geom_point()+theme_bw()+ylab("True Positive Rate (sensitivity)")+xlab("False Positive Rate (1-specificity)")
-g<-g+geom_hline(yintercept = 0.9)
+  geom_point()+theme_bw()+ylab("True Positive Rate (specificity)")+xlab("False Positive Rate (1-sensitivity)")
 g
+
 #pROC package prefers true and false to be 1s and 0s, multiplying by one does this
 data_mod$overall_significance<-data_mod$overall_significance*1
 #use roc function from the overall significance and the results of the screen
@@ -136,50 +130,3 @@ roc_obj<-roc(data_mod$overall_significance,abs(data_mod$monothermal_screen),na.r
 auc(roc_obj)
 #plot ROC
 plot(roc_obj)
-
-
-
-
-#TEST 20
-significance=20
-#boolean- is absolute value above significance value
-data_mod$mono_significance<-abs(data_mod$monothermal_screen)>=significance
-
-# for the rows of data_mod where overall significance is true AND mono significance is true - set rating to TP
-data_mod[which(data_mod$overall_significance&data_mod$mono_significance),"rating"]<-"TP"
-# for the rows of data_mod where overall significance is NOT true (!) AND mono significance is true - set rating to FP
-data_mod[which(!data_mod$overall_significance&data_mod$mono_significance),"rating"]<-"FP"
-# for the rows of data_mod where overall significance is true AND mono significance is NOT true - set rating to FN
-data_mod[which(data_mod$overall_significance&!data_mod$mono_significance),"rating"]<-"FN"
-# for the rows of data_mod where overall significance is NOT true (!) AND mono significance is NOT true - set rating to TN
-data_mod[which(!data_mod$overall_significance&!data_mod$mono_significance),"rating"]<-"TN"
-#calculate sensitivity  and specificity from our ratings column
-sensitivity<-sum(data_mod$rating=="TP",na.rm = T)/(sum(data_mod$rating=="TP",na.rm = T)+sum(data_mod$rating=="FN",na.rm = T))
-specificity<-sum(data_mod$rating=="TN",na.rm = T)/(sum(data_mod$rating=="TN",na.rm = T)+sum(data_mod$rating=="FP",na.rm = T))
-#return sensitivity and specificity
-sum(data_mod$rating=="FN",na.rm = T)
-sum(data_mod$rating=="TN",na.rm = T)
-sum(data_mod$rating=="FP",na.rm = T)
-sum(data_mod$rating=="TP",na.rm = T)
-
-
-a<-glm(overall_significance~abs(monothermal_screen),family=binomial,data=data_mod)
-summary(a)
-x=60
-p=(exp(a$coefficients[1]+a$coefficients[2]*x))/(1+exp(a$coefficients[1]+a$coefficients[2]*x))
-p
-exp(1*a$coefficients[2])
-
-
-data_mod_short<-data.frame(monothermal_screen=data_mod$monothermal_screen)
-predicted_prob<-predict(a,data_mod_short,type="response")
-pred_cut<-ifelse(predicted_prob>0.5,1,0)
-
-classDF<-data.frame(response=data_mod$overall_significance,predicted=pred_cut)
-xtabs(~predicted+response,data=classDF)
-
-return(list(sens_spec_dataset,g))
-#how to get from predicted_prob to  cut off monothermal screen cut off
-}
-
-likelihood_ratio_dataset(data_mod)
